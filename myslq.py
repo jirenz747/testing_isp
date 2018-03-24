980962427d3f642d9d955b8160ac85bdcb4a93e5
jirenz747 <wreck747@gmail.com>
2018-03-24 22:00:59 +0300

    удлалил комменты

--- myslq.py
+++ myslq.py
@@ -24,7 +24,3 @@ def sql_query_all(query):
         return False
     else:
         return answer
-
-# answer = sql_query("SELECT * FROM shops where shop_obj = 'mos2-gw'")
-#answer = sql_query(f'select * from providers t1 inner join shops t2 on t1.shop_id = t2.shop_id where t2.shop_obj ="ptg2-gw" and t1.status_problem = 0 and t1.our_ip = "172.16.41.9"')
-#print(answer)
