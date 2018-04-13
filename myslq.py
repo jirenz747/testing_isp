5824a25289a3cfbe8d8df1adfc4596c87a697729
jirenz747 <37732694+jirenz747@users.noreply.github.com>
2018-04-13 11:49:16 +0300

    Update myslq.py

--- myslq.py
+++ myslq.py
@@ -2,7 +2,7 @@ import MySQLdb
 from passwords import get_mysql_password
 
 MYSQL_USER, MYSQL_PASS, MYSQL_SERVER = get_mysql_password()
-connection = MySQLdb.connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, 'shops_test', charset="utf8", use_unicode=True)
+connection = MySQLdb.connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, 'shops', charset="utf8", use_unicode=True)
 cursor = connection.cursor()
 
 
